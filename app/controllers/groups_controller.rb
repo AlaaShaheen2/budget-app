bcb8401dfff8963003d7888f634a3a3c45d9c247
AlaaShaheen2 <alaa.m.shaheen22@gmail.com>
2023-02-24 21:59:25 +0430

    add expenses to the routes and fix total expenses

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,12 +1,13 @@
 class GroupsController < ApplicationController
   before_action :find_user
-  before_action :find_group, only: %i[show edit update destroy]
+  before_action :find_group, only: [:show, :edit, :update, :destroy]
 
   def index
     @groups = @user.groups.all
   end
 
-  def show; end
+  def show
+  end
 
   def new
     @group = Group.new
@@ -16,14 +17,15 @@ class GroupsController < ApplicationController
     @group = Group.new(group_params)
     @group.user = @user
     if @group.save
-      redirect_to group_path(id: @group.id), notice: 'Group created successfully!'
+      redirect_to group_path(id: @group.id), notice: 'Group created successfully'
     else
       flash.now[:alert] = @group.errors.full_messages.first if @group.errors.any?
       render :new, status: 400
     end
   end
 
-  def edit; end
+  def edit
+  end
 
   def update
     if @group.update(group_params)
@@ -35,6 +37,13 @@ class GroupsController < ApplicationController
   end
 
   def destroy
+    @group = Group.find(params[:id])
+    @group_expenses = GroupExpense.where(group_id: @group.id)
+    @group_expenses.each do |group_expense|
+      expense_id = group_expense.expense_id
+      group_expense.destroy
+      expense = Expense.delete(expense_id)
+    end
     if @group.destroy
       redirect_to groups_path, notice: 'Groups was deleted successfully'
     else
